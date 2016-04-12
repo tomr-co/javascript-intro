1c6f3832616ba725d7a12ea44ea23d57121fa1ce
tom richardson <tomr.design@gmail.com>
2016-04-13 10:01:57 +1200

    start validation code

---
+++ js/validation.js
@@ -0,0 +1,18 @@
+//find the form
+var registrationForm = document.querySelector("#registration");
+
+//Wait for the user to submit the form
+registrationForm.onsubmit = function(event) {
+
+	// stop the form from submitting
+	event.preventDefault();
+
+}
+
+
+
+
+
+
+
+
